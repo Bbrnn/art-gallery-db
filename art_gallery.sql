17eddaecf85dee5b31b7c343d478fb75b528ae28
bridget <113863725+Bbrnn@users.noreply.github.com>
2025-05-15 09:11:12 +0300

    Create art_gallery.sql

---
+++ art_gallery.sql
@@ -0,0 +1,5 @@
+-- Art Gallery Inventory & Sales Database
+-- Author: Bridget Ngugi
+-- Description: SQL schema for managing artworks, artists, exhibitions, sales, and buyers
+
+-- Drop tables if they exist (for re-import)
